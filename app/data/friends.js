71c59cd1da8b5a8fd461f83a6e8c1b8a3a45a3ac
ajmesa <amesa987@gmail.com>
2019-11-06 14:44:34 -0500

    Survey Change

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,22 @@
 //Current friend data
 var friendData = [
   {
-    name: "Ahmed",
+    name: "Alan",
     photo:
-      "https://i.pinimg.com/originals/b3/e1/7b/b3e17b4fdcf0067ea0a79e4c5510ed20.jpg",
-    scores: [5, 3, 4, 4, 3, 1, 2, 5, 1, 5]
+      "https://media.gamestop.com/i/gamestop/10138868/Xbox-One-S-Deep-Blue-Special-Edition-500GB?$zoom$",
+    scores: [5, 3, 4, 4, 3, 1, 2, 5, 1, 3]
   },
   {
     name: "Courtney",
     photo:
-      "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    scores: [5, 1, 1, 1, 1, 5, 3, 1, 5, 4]
+      "https://cdn.theatlantic.com/assets/media/img/mt/2017/10/Pict1_Ursinia_calendulifolia/lead_720_405.jpg?mod=1533691909",
+    scores: [5, 1, 1, 1, 1, 5, 3, 1, 5, 1]
   },
   {
     name: "Bob",
     photo:
-      "https://www.menshairstyletrends.com/wp-content/uploads/2018/04/hayden_cassidy-taper-haircut-textured-hairstyles-for-men-blow-dryer.jpg",
-    scores: [2, 5, 3, 5, 4, 2, 4, 5, 4, 1]
+      "https://dks.scene7.com/is/image/dkscdn/16NISWNSHKPBL15LXRMB_Green_Teal_Black_is?wid=1080&fmt=jpg",
+    scores: [2, 5, 3, 5, 4, 2, 4, 5, 4, 5]
   }
 ];
 
